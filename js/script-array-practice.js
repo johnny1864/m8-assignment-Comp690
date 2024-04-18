1eb6ea3ef49066be2b10dabde03077c56340cb82
unknown <j.rivera@powerdigitalmarketing.com>
2024-04-17 18:51:48 -0700

    finished steps

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -35,21 +35,67 @@ delete moviesArr2[0];
 console.log(moviesArr2);
 //STEP 5
 
+let moviesArr3 = ["The Matrix", "Dune", "Avatar", "Godzilla", "King Kong", "Jaws", "Fallout"];
+
+for(let movie in moviesArr3){
+    console.log(moviesArr3[movie]);
+}
+
 //STEP 6
+moviesArr3 = moviesArr3.sort();
+for (let i of moviesArr3) {
+    console.log(i);
+}
 
 //STEP 7
 
+for (let i of moviesArr3) {
+    console.log(i);
+}
+
 //STEP 8
 
+let leastFavMovies = ["Notebook", "Battleship", "Starwar Episode 3"];
+
+console.log("Movies I like: \n", "\n");
+for (let movie of moviesArr3) {
+    console.log(movie);
+}
+
+console.log("Movies I regret watching: \n", "\n");
+for (let movie of leastFavMovies) {
+    console.log(movie);
+}
+
 //STEP 9
+moviesArr3 = moviesArr3.concat(leastFavMovies).sort().reverse();
+console.log(moviesArr3);
 
 //STEP 10
 
+let lastMovie = moviesArr3.pop();
+console.log(lastMovie);
+
 //STEP 11
+
+let firstMovie = moviesArr3.unshift();
+console.log(firstMovie);
 
 //STEP 12
 
+let arr = [58, '', 'abcd', true, null, false, 0];
+
+arr = arr.map(el => {
+    if(el){
+        return el;
+    }
+});
+
+console.log(arr);
+
 //STEP 13
+
+
 
 //STEP 14
 
